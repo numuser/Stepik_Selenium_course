8cac904cf99a00a5673f7968391c433492d7f84a
Nikita V <vyatchanin.11@gmail.com>
2023-05-10 16:05:12 +0500

    Update test_fixture_xfail.py

--- SeleniumAutotests/3_5_marking_fixture/test_fixture_xfail.py
+++ SeleniumAutotests/3_5_marking_fixture/test_fixture_xfail.py
@@ -15,6 +15,7 @@ def browser():
     browser.quit()
 
 
+@pytest.mark.mainpage
 class TestMainPage1():
 
     def test_guest_should_see_login_link(self, browser):
@@ -30,3 +31,18 @@ class TestMainPage1():
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, "button.favorite")
 
+
+@pytest.mark.otherpage
+class TestOtherPage1():
+
+    @pytest.mark.xfail(strict=True)
+    def test_succeed(self):
+        assert True
+
+    @pytest.mark.xfail()
+    def test_not_succeed(self):
+        assert False
+
+    @pytest.mark.skip
+    def test_skipped(self):
+        assert False
