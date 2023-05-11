6935aa54ec6b94da588102490ddb09ee2a9518c2
Nikita V <vyatchanin.11@gmail.com>
2023-05-11 14:31:11 +0500

    Update test_conftest.py

--- SeleniumAutotests/3_6_PyTest_conf_plugs_parametrs/test_conftest.py
+++ SeleniumAutotests/3_6_PyTest_conf_plugs_parametrs/test_conftest.py
@@ -4,6 +4,11 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/"
 
 
+# Для хранения часто употребимых фикстур и хранения глобальных настроек нужно использовать файл conftest.py,
+# который должен лежать в директории верхнего уровня в вашем проекте с тестами.
+# Можно создавать дополнительные файлы conftest.py в других директориях,
+# но тогда настройки в этих файлах будут применяться только к тестам в под-директориях.
+
 def test_guest_should_see_login_link(browser):
     browser.get(link)
     browser.find_element(By.CSS_SELECTOR, "#login_link")
