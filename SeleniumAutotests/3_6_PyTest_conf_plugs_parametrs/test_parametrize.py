1e8f02986d4c189266e7508c0423d4825e06ac40
Nikita V <vyatchanin.11@gmail.com>
2023-05-11 14:40:52 +0500

    Update test_parametrize.py

--- SeleniumAutotests/3_6_PyTest_conf_plugs_parametrs/test_parametrize.py
+++ SeleniumAutotests/3_6_PyTest_conf_plugs_parametrs/test_parametrize.py
@@ -8,8 +8,24 @@ from selenium.webdriver.common.by import By
 
 # В @pytest.mark.parametrize() нужно передать параметр, который должен изменяться, и список значений параметра.
 
+
 @pytest.mark.parametrize("language", ["ru", "en-gb"])
 def test_login_with_languages(browser, language):
     link = f"http://selenium1py.pythonanywhere.com/{language}/"
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "#login_link"
+    browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+# Можно задавать параметризацию также для всего тестового класса,
+# чтобы все тесты в классе запустились с заданными параметрами.
+# В таком случае отметка о параметризации должна быть перед объявлением класса:
+
+
+@pytest.mark.parametrize("language", ["ru", "en-gb"])
+class TestLogin():
+    def test_guest_should_see_login_link(self, browser, language):
+        link = f"http://selenium1py.pythonanywhere.com/{language}/"
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    def test_guest_should_see_navbar_element(self, browser, language):
+        pass
