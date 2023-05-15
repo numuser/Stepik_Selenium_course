b08e08e72bbe5b5580fb57da5bc7689e9d267736
Nikita V <vyatchanin.11@gmail.com>
2023-05-15 15:16:37 +0500

    Improved to work with firefox

--- SeleniumAutotests/conftest.py
+++ SeleniumAutotests/conftest.py
@@ -1,11 +1,15 @@
 import pytest
 from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 @pytest.fixture(scope="function")
-def browser():
+def browser(driver="chrome"):
     print("\nstart browser for test ... ")
-    browser = webdriver.Chrome()
+    if driver == "chrome":
+        browser = webdriver.Chrome()
+    elif driver == "firefox":
+        browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
     browser.implicitly_wait(5)
     yield browser
     print("\nquit browser ... ")
