0014836a2bbe32c696b88df85c13a816709eb24a
Nikita V <vyatchanin.11@gmail.com>
2023-05-15 19:02:49 +0500

    Update test_items.py

--- SeleniumAutotests/For_the_reviewer/test_items.py
+++ SeleniumAutotests/For_the_reviewer/test_items.py
@@ -8,5 +8,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_visible_add_basket_button(browser):
     browser.get(link)
+    time.sleep(30)
     assert browser.find_element(By.CLASS_NAME, "btn-add-to-basket"), "Button not found!"
-    time.sleep(20)
