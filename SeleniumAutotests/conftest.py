8c4adac4f654d65a86fd8b49d85baaaba7883283
Nikita V <vyatchanin.11@gmail.com>
2023-05-15 15:57:24 +0500

    Improved conftest.py to work with Firefox

--- SeleniumAutotests/conftest.py
+++ SeleniumAutotests/conftest.py
@@ -1,16 +1,35 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service as FoxService
 from webdriver_manager.firefox import GeckoDriverManager
 
 
+def pytest_addoption(parser):
+    parser.addoption('--browser_name',
+                     action='store',
+                     default='chrome',
+                     help="Choose browser: chrome or firefox"
+                     )
+
+
 @pytest.fixture(scope="function")
-def browser(driver="chrome"):
-    print("\nstart browser for test ... ")
-    if driver == "chrome":
-        browser = webdriver.Chrome()
-    elif driver == "firefox":
-        browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-    browser.implicitly_wait(5)
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart Chrome browser for test ... ")
+        browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    elif browser_name == "firefox":
+        print("\nstart Firefox browser for test ... ")
+        browser = webdriver.Firefox(service=FoxService(GeckoDriverManager().install()))
+
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser ... ")
     browser.quit()
+
+
