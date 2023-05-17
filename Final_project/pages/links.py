a20b3aaf3be5a2a22f864f85f725405c82d3dc81
Nikita V <vyatchanin.11@gmail.com>
2023-05-17 21:53:59 +0500

    move links in a separate file

---
+++ Final_project/pages/links.py
@@ -0,0 +1,5 @@
+class PageLinks():
+    MAIN_PAGE_URL = "https://selenium1py.pythonanywhere.com/"
+    LOGIN_PAGE_URL = "https://selenium1py.pythonanywhere.com/accounts/login/"
+
+
--- Final_project/test_main_page.py
+++ Final_project/test_main_page.py
@@ -1,18 +1,17 @@
 # Flags:
 # -v --tb=line --language=en
 from .pages.main_page import MainPage
-
-link = "https://selenium1py.pythonanywhere.com/"
+from .pages.links import PageLinks
 
 
 def test_guest_can_go_to_login_page(browser):
     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page = MainPage(browser, link)
+    page = MainPage(browser, PageLinks.MAIN_PAGE_URL)
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
 
 
 def test_should_see_login_link(browser):
-    page = MainPage(browser, link)
+    page = MainPage(browser, PageLinks.MAIN_PAGE_URL)
     page.open()
     page.should_be_login_link()  # выполняем метод страницы — проверяем видимость кнопки логина
