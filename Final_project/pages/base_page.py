2417aa76a1a501d78498be2167ab2578168eeda0
Nikita V <vyatchanin.11@gmail.com>
2023-05-17 20:00:58 +0500

    add check for element visibility

--- Final_project/pages/base_page.py
+++ Final_project/pages/base_page.py
@@ -1,7 +1,23 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage():
-    def __init__(self, browser, link):
+    def __init__(self, browser, link, timeout=10):
         self.browser = browser
         self.link = link
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.link)
+
+    def is_element_present(self, how_search, what_search: str):
+        """
+        :param how_search: By.CSS_SELECTOR, By.ID, etc.
+        :param what_search: Selector
+        :return: bool
+        """
+        try:
+            self.browser.find_element(how_search, what_search)
+        except NoSuchElementException:
+            return False
+        return True
