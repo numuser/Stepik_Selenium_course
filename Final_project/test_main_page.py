02cf6ca7b1264041a1bf332b67587cb4b18a752b
Nikita V <vyatchanin.11@gmail.com>
2023-05-17 20:02:00 +0500

    Update test_main_page.py

--- Final_project/test_main_page.py
+++ Final_project/test_main_page.py
@@ -10,3 +10,11 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+
+
+def test_should_see_login_link(browser):
+    link = "https://selenium1py.pythonanywhere.com/"
+
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
