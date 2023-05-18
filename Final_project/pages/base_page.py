ba563543dee63665cc01f63ee33b03122a50391e
Nikita V <vyatchanin.11@gmail.com>
2023-05-18 16:57:01 +0500

    Update base_page.py

--- Final_project/pages/base_page.py
+++ Final_project/pages/base_page.py
@@ -1,4 +1,6 @@
+import math
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
 
 
 class BasePage():
@@ -17,7 +19,36 @@ class BasePage():
         :return: bool
         """
         try:
+            self.browser.implicitly_wait(5)
             self.browser.find_element(how_search, what_search)
         except NoSuchElementException:
+            return False
+        return True
+
+    def is_element_clickable(self, how_search, what_search):
+        """
+        :param how_search: By.CSS_SELECTOR, By.ID, etc.
+        :param what_search: Selector
+        :return: bool
+        """
+        try:
+            self.browser.implicitly_wait(5)
+            self.browser.find_element(how_search, what_search).click()
+        except NoSuchElementException:
+            return False
+        return True
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            print(f"Your code: {alert.text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
             return False
         return True
