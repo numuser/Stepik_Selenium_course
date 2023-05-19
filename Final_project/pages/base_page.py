b34486f0323ae50b2cc3d3c58e62abdd48759168
Nikita V <vyatchanin.11@gmail.com>
2023-05-19 15:48:46 +0500

    refactor main_page methods -> base_page methods

--- Final_project/pages/base_page.py
+++ Final_project/pages/base_page.py
@@ -1,7 +1,9 @@
 import math
 from selenium.webdriver import Remote
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 
 
 class BasePage():
@@ -23,6 +25,13 @@ class BasePage():
         """
         self.browser.get(self.link)
 
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
     def is_element_present(self, how_search, what_search: str):
         """
         :param how_search: By.CSS_SELECTOR, By.ID, etc.
@@ -33,6 +42,41 @@ class BasePage():
             self.browser.implicitly_wait(5)
             self.browser.find_element(how_search, what_search)
         except NoSuchElementException:
+            return False
+        return True
+
+    def is_not_element_present(self, how_search, what_search, timeout=4):
+        """
+
+        :param how_search: By.CSS_SELECTOR, By.ID, etc.
+        :param what_search: Selector
+        :param timeout: Time to wait before raise in seconds
+        :return: bool
+        """
+        try:
+            WebDriverWait(
+                self.browser, timeout).until(
+                EC.presence_of_element_located((how_search, what_search))
+            )
+
+        except TimeoutException:
+            return True
+        return False
+
+    def is_disappeared(self, how_search, what_search, timeout=4):
+        """
+
+        :param how_search: By.CSS_SELECTOR, By.ID, etc.
+        :param what_search: Selector
+        :param timeout: Time to wait before raise in seconds
+        :return: bool
+        """
+        try:
+            WebDriverWait(
+                self.browser, timeout, 1, TimeoutException
+            ).until_not(EC.presence_of_element_located((how_search, what_search))
+                        )
+        except TimeoutException:
             return False
         return True
 
@@ -63,5 +107,5 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("\nNo second alert presented")
-            return True
+            return False
         return True
--- Final_project/pages/locators.py
+++ Final_project/pages/locators.py
@@ -1,8 +1,18 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
+class BookPageLocators():
+    ADD_TO_BASKET_BTN = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success")
+    BOOK_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    BOOK_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    BASKET_ITEM_NAME = (By.CSS_SELECTOR, ".alertinner strong")
+    BASKET_TOTAL_PRICE = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
 
 
 class LoginPageLocators():
@@ -11,9 +21,5 @@ class LoginPageLocators():
     REGISTER_FORM = (By.ID, "register_form")
 
 
-class BookPageLocators():
-    ADD_TO_BASKET_BTN = (By.CSS_SELECTOR, ".btn-add-to-basket")
-    BOOK_NAME = (By.CSS_SELECTOR, ".product_main h1")
-    BOOK_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
-    BASKET_ITEM_NAME = (By.CSS_SELECTOR, ".alertinner strong")
-    BASKET_TOTAL_PRICE = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
